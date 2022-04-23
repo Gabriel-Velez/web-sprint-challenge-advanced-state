c0c7e2aa088e335cddc5073d3c569050e8d31af3
gabriel velez <gabriel.dan.velez@gmail.com>
2022-04-23 04:30:50 -0400

    .

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,17 +1,20 @@
+import * as types from "./action-types";
+import axios from "axios";
+
 // ❗ You don't need to add extra action creators to achieve MVP
-export function moveClockwise() { }
+export function moveClockwise() {}
 
-export function moveCounterClockwise() { }
+export function moveCounterClockwise() {}
 
-export function selectAnswer() { }
+export function selectAnswer() {}
 
-export function setMessage() { }
+export function setMessage() {}
 
-export function setQuiz() { }
+export function setQuiz() {}
 
-export function inputChange() { }
+export function inputChange() {}
 
-export function resetForm() { }
+export function resetForm() {}
 
 // ❗ Async action creators
 export function fetchQuiz() {
@@ -19,21 +22,36 @@ export function fetchQuiz() {
     // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
     // On successful GET:
     // - Dispatch an action to send the obtained quiz to its state
-  }
+    dispatch({ type: types.SET_QUIZ_INTO_STATE, payload: null });
+    axios
+      .get("http://localhost:9000/api/quiz/next")
+      .then((res) => {
+        console.log(res.data);
+        dispatch({ type: types.SET_QUIZ_INTO_STATE, payload: res.data });
+      })
+      .catch((err) => console.error(err));
+  };
 }
-export function postAnswer() {
+export function postAnswer(answer) {
   return function (dispatch) {
     // On successful POST:
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
-  }
+
+    axios
+      .post("http://localhost:9000/api/quiz/new", { answer: answer })
+      .then((res) => {
+        dispatch({ type: types.SET_QUIZ_INTO_STATE, payload: res.data });
+      })
+      .catch((err) => console.error(err));
+  };
 }
 export function postQuiz() {
   return function (dispatch) {
     // On successful POST:
     // - Dispatch the correct message to the the appropriate state
     // - Dispatch the resetting of the form
-  }
+  };
 }
 // ❗ On promise rejections, use log statements or breakpoints, and put an appropriate error message in state
